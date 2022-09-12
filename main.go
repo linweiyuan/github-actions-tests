b40161e52dd3b569598bc25ce724be024f408641
linweiyuan <root@linweiyuan.com>
2022-09-13 01:46:54 +0800

    re-deploy testing

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello world")
+		fmt.Fprintf(w, "hello world update and re-deploy")
 	})
 
 	http.ListenAndServe(":8080", nil)
